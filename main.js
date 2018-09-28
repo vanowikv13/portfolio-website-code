b4f7386d44e5a6ff845c4bed27614791088c1026
vanowikv13 <vanowikv13@gmail.com>
2018-09-28 12:08:15 +0200

    really big improvment

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+//hide navbar at the begining
+$(".navbar").hide();
+
+//variable checking status for the navbar function
+var begining = false;
+
+//point of status where to hide element
+var point = 222;
+
+//write width of the website
+console.log(window.pageXOffset);
+
+//show navbar when it's on the bootom and under image
+$(document).ready(function () {
+    //hide scroll bar on top
+    window.onscroll = function () {
+        let currentScrollPos = window.pageYOffset;
+        if (currentScrollPos < point)
+            $('.navbar').hide(1000);
+        else {
+            if (currentScrollPos >= point) {
+                if (begining == false) {
+                    $('.navbar').delay(1200).show(1000);
+                    begining = true;
+                } else
+                    $('.navbar').show(1000);
+            }
+        }
+    }
+});
+
+//smoothy scrolling
+$(document).ready(function() {
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $( $(this).attr('href') );
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 800);
+		}
+    });
+});
+
+
+
 //download pdf
 function clickDow(name = 'cv.pdf') {
     var link = document.createElement('a');
@@ -13,49 +59,3 @@ function clickView(name = 'cv.pdf') {
     link.target = '_blank'
     link.dispatchEvent(new MouseEvent('click'));
 }
-
-//hide navbar at the begining
-$(".navbar").hide();
-
-//variable checking status for the navbar function
-var begining = false;
-
-//point of status where to hide element
-var point = 222;
-
-//show navbar when it's on the bootom and under image
-$(document).ready(function () {
-    //hide scroll bar on top
-    window.onscroll = function () {
-        let currentScrollPos = window.pageYOffset;
-        ///console.log(currentScrollPos);
-        ///console.log(begining);
-        if (currentScrollPos < point)
-            $('.navbar').hide(1000);
-        else {
-            if (window.pageYOffset >= point) {
-                if (begining == false) {
-                    $('.navbar').delay(1200).show(1000);
-                    begining = true;
-                } else
-                    $('.navbar').show(1000);
-            }
-        }
-    }
-});
-
-//smooth scrolling
-$(document).ready(function() {
-	$('a[href^="#"]').on('click', function(event) {
-		var target = $( $(this).attr('href') );
-		if( target.length ) {
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top
-			}, 800);
-		}
-	});
-
-});
-
-
