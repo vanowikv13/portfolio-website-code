7473c55b5e68f0410a183c7309b2a0fc63b8156e
vanowikv13 <vanowikv13@gmail.com>
2018-09-30 13:15:41 +0200

    RWD with navbar and other small improvments

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 //hide navbar at the begining
 $(".navbar").hide();
 
-//show and hide navbar button in right moment
+//show and hide navbar button on small width in right moment
 function navBtn() {
-    if(document.documentElement.clientWidth <= 560)
+    if ($( window ).width() <= 560)
         $('#btnNav').show();
     else
         $('#btnNav').hide();
 }
 
-$(document).ready(function (){
+$(document).ready(function () {
     navBtn();
-    $(window).resize(function() {
+    $(window).resize(function () {
         navBtn();
     })
 });
@@ -19,20 +19,21 @@ $(document).ready(function (){
 //variable checking status for the navbar function
 var begining = false;
 
-//point of status where to hide element
+//start point of Y offset when hide elements
 var point = 222;
 
+//last position of scroll
+var lastScrollTop = 0;
 
-
-//show navbar when it's on the bootom and under image
+//navbar scripts
 $(document).ready(function () {
-    //hide scroll bar on top
+    //hide nav on top
     window.onscroll = function () {
         let currentScrollPos = window.pageYOffset;
         if (currentScrollPos < point)
             $('.navbar').hide(1000);
         else {
-            if (currentScrollPos >= point) {
+            if (currentScrollPos >= point && $( window ).width() > 575) {
                 if (begining == false) {
                     $('.navbar').delay(1200).show(1000);
                     begining = true;
@@ -40,26 +41,36 @@ $(document).ready(function () {
                     $('.navbar').show(1000);
             }
         }
+
+        //on small resoulution scrolling
+        if ($( window ).width() <= 575) {
+            if (currentScrollPos > lastScrollTop) {
+                $('.navbar').hide();
+            } else {
+                $('.navbar').show();
+            }
+            lastScrollTop = currentScrollPos;
+        }
     }
 });
 
 //smoothy scrolling
-$(document).ready(function() {
-	$('a[href^="#"]').on('click', function(event) {
-		var target = $( $(this).attr('href') );
-		if( target.length ) {
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top
-			}, 800);
-		}
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
     });
 });
 
-
+//download and view cv depends of arguments
 function cvAction(name = 'polish.pdf', download = true) {
     var link = document.createElement('a');
-    if(download) {
+    if (download) {
         link.href = 'pdf/';
         link.download = name;
     } else {
